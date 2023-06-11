7adb1a27e9b4da7bf2be037502f19fb9c94a32aa
Dhruv Shah <dhruvshahrds@gmail.com>
2023-06-11 15:29:03 +0530

    feat: endpoints for events with bulk upload

--- student_welfare_backend/student_welfare_backend/core/api/urls.py
+++ student_welfare_backend/student_welfare_backend/core/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from student_welfare_backend.core.api.views.clubs import ClubViewSet, ClubAdminViewSet, ClubsListView, ClubBulkUploadView
+from student_welfare_backend.core.api.views.events import EventViewSet, EventAdminViewSet, EventBulkUploadView
+
 
 # URL PATTERNS
 urlpatterns = [
@@ -8,6 +10,7 @@ urlpatterns = [
     path("clubs/titles/", ClubsListView.as_view(), name="clubs_titles"),
     # BULK UPLOAD
     path('admin/clubs/bulk_upload/', ClubBulkUploadView.as_view(), name='clubs_bulk_upload'),
+    path('admin/events/bulk_upload/', EventBulkUploadView.as_view(), name='events_bulk_upload'),
 ]
 
 # CLUBS
@@ -18,3 +21,13 @@ urlpatterns += club_router.urls
 club_admin_router = DefaultRouter()
 club_admin_router.register(r'admin/clubs', ClubAdminViewSet, basename='clubs_admin')
 urlpatterns += club_admin_router.urls
+
+
+# EVENTS
+event_router = DefaultRouter()
+event_router.register(r'events', EventViewSet, basename='events')
+urlpatterns += event_router.urls
+
+event_admin_router = DefaultRouter()
+event_admin_router.register(r'admin/events', EventAdminViewSet, basename='events_admin')
+urlpatterns += event_admin_router.urls
