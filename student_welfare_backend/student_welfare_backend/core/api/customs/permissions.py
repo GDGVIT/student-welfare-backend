1b313e0f4942e6e34cc1f045089fec12dfbe48f9
Dhruv Shah <dhruvshahrds@gmail.com>
2023-06-11 14:36:08 +0530

    feat: custom paginations and permissions

---
+++ student_welfare_backend/student_welfare_backend/core/api/customs/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+
+
+# Faculty field of User model is used to determine whether the user is a faculty or not
+class IsFaculty(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_faculty
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_faculty
+
+
+# IsDSW field to see if user in DSW or not
+class IsDSW(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_dsw
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_dsw
