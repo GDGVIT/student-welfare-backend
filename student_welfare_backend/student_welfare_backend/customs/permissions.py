92f06fce7e9ffdd3449b609fc1f2a280e82b6ead
Dhruv Shah <dhruvshahrds@gmail.com>
2024-03-12 21:28:13 +0530

    feat: create customs module

---
+++ student_welfare_backend/student_welfare_backend/customs/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import BasePermission
+
+
+# Faculty field of User model is used to determine whether the user is a faculty or not
+class IsFaculty(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_faculty
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_faculty
+
+
+# IsDSW field to see if user in DSW or not
+class IsDSW(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_dsw
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_dsw
+
+
+# IsADSW field to see if user in ADSW or not
+class IsADSW(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_adsw
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_adsw
---
+++ student_welfare_backend/student_welfare_backend/customs/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from student_welfare_backend.customs.permissions import IsDSW, IsADSW
+from student_welfare_backend.csv_handler.importer import CSVImporter
+from student_welfare_backend.csv_handler.exporter import CSVExporter
+
+
+class BaseAdminSWView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsDSW, IsADSW]
+
+
+class BaseBulkUploadView(BaseAdminSWView):
+    importer_class = CSVImporter
+    csv_type = None
+
+    def post(self, request):
+        csv_file = request.FILES.get("file")
+        if not csv_file:
+            return Response({"error": "No file found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        importer = self.importer_class(self.csv_type)
+        success, responses = importer.process_csv(csv_file)
+        
+        if success:
+            return Response(responses, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": responses}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BaseBulkDownloadView(BaseAdminSWView):
+    exporter_class = CSVExporter
+    csv_type = None
+
+    def get(self, request):
+        exporter = CSVExporter(self.csv_type)
+        csv = exporter.export_csv(request.query_params.get("filter", {}))
+
+       # Create an HTTP response with CSV data
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="export.csv"'
+
+        # Write CSV data to the response
+        response.write(csv.getvalue())
+        return response
