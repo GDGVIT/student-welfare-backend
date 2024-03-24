c179a9be048f67618111b34b7764238ae5b56b68
rb-25 <aarabibalakrishnan2@gmail.com>
2024-03-24 21:16:58 +0530

    feat: added changes as per review

--- student_welfare_backend/student_welfare_backend/csv_handler/importer/newsletters.py
+++ student_welfare_backend/student_welfare_backend/csv_handler/importer/newsletters.py
@@ -21,17 +21,17 @@ class NewslettersCSVImporter:
             # Validate required fields
             missing_fields = [field for field in self.REQUIRED_FIELDS if row_data[field] is None]
             if missing_fields:
-                raise ValueError(f"Missing required fields: {', '.join(missing_fields)}")
+                raise ValueError(f"Missing required fields: {', '.join(missing_fields)}")            
+      
+            # Check if the month is valid accoring to role choices
+            if row_data["month"].lower() not in dict(Newsletter.month_names).keys():
+                invalid_month = row_data["month"].lower()
+                raise ValueError(f"Invalid type: {invalid_month}")
             
-            month1 = row_data["month"].lower()
-        
-            # Check if the type is valid
-            if month1 not in dict(Newsletter.month_names).keys():
-                raise ValueError(f"Invalid type: {month1}")
             # Create special file
             newsletter = Newsletter.objects.update_or_create(
                 year=row_data["year"],
-                month=month1,
+                month=row_data["month"].lower(),
                 cover_page=row_data["cover_page"],
                 link=row_data["link"],
             )
