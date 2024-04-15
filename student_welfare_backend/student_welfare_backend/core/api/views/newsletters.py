dbddfc9bc02b5623161d8606aa24ebfe9998e5ed
Dhruv Shah <dhruvshahrds@gmail.com>
2024-04-16 02:33:29 +0530

    chore: attempt to rightly order newsletters

--- student_welfare_backend/student_welfare_backend/core/api/views/newsletters.py
+++ student_welfare_backend/student_welfare_backend/core/api/views/newsletters.py
@@ -1,4 +1,6 @@
-from rest_framework import status
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
+from rest_framework import status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
@@ -6,6 +8,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
+
 
 
 from student_welfare_backend.core.models import Newsletter
@@ -21,11 +24,21 @@ class NewsletterViewSet(ReadOnlyModelViewSet):
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
     pagination_class = CustomPagination
-    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     filterset_fields = ["year", "month"]
     search_fields = ["year", "month"]
     ordering_fields = ["year", "month"]
-    ordering = ["-year", "-month"]
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        
+        # Custom ordering: Combine year and month into a numeric value for sorting
+        queryset = queryset.annotate(
+            year_as_int=Cast('year', IntegerField()),
+            month_as_int=Cast('month', IntegerField())
+        ).order_by('-year_as_int', '-month_as_int')
+        
+        return queryset
 
 
 class NewsletterAdminViewSet(ModelViewSet):
