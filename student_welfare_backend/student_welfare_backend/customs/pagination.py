0dcb342dd0761ff1bc48da3941c311569fe3de16
Aarabi B <68107896+rb-25@users.noreply.github.com>
2024-05-03 16:27:47 +0530

    feat: added min_year to newsletter response (#9)
    * feat: endpoint to add user to org, school field in user
    
    * feat: changed manage user view
    
    * feat: modified try except block
    
    * feat: removed User and Org db instances
    
    * feat: added min year to newsletter response

--- student_welfare_backend/student_welfare_backend/customs/pagination.py
+++ student_welfare_backend/student_welfare_backend/customs/pagination.py
@@ -1,7 +1,25 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
+from student_welfare_backend.core.models import Newsletter
+from django.db.models import Min
 
 class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "size"
     max_page_size = 1000
+
+class NewsletterPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "size"
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        min_year = Newsletter.objects.aggregate(Min('year')).get('year__min')
+        response = super().get_paginated_response(data)
+        return Response({
+            'min_year': min_year,
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
