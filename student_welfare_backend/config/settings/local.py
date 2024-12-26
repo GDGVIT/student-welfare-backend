d7cd6c3ac59e6b675cdb8d51cee39669a5ab66be
Dev Keshwani <dev.keshwani345@gmail.com>
2024-12-26 16:03:49 +0530

    fix(notifications): fcm based approach to send notifications

--- student_welfare_backend/config/settings/local.py
+++ student_welfare_backend/config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="w2KfDUzhIeBnseruBGtYHi7wYqcov7mtEP5E690tQEQvJY8jqgpDhPrwu2gP8vxV",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","10.0.2.2"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- student_welfare_backend/student_welfare_backend/core/tasks/notifications.py
+++ student_welfare_backend/student_welfare_backend/core/tasks/notifications.py
@@ -2,24 +2,62 @@ import firebase_admin
 from firebase_admin import messaging, credentials
 from config.celery_app import app
 from django.utils import timezone
-
+import logging
 from student_welfare_backend.core.models import Event
+from django.contrib.auth import get_user_model
+
 
 cred = credentials.Certificate("/app/credentials/firebase-key.json")
 firebase_app = firebase_admin.initialize_app(cred)
 
+print("Firebase app initialized.", firebase_app)
 
 def get_title(event: Event):
     return f"Register for {event.name} by {event.organization.name} on VTOP now!"
 
+
 @app.task(name="send_notification")
-def send_notification(title, body, topic, image_url=None):
-    message = messaging.Message(
-        notification=messaging.Notification(title=title, body=body, image=image_url),
-        topic=f"/topics/{topic.replace(' ', '_').lower()}",
+def send_notification(title, body, topic=None, image_url=None):
+    User = get_user_model()
+    tokens = User.objects.exclude(fcm_token__isnull=True).exclude(fcm_token="").values_list("fcm_token", flat=True)
+    tokens = list(tokens)
+    if not tokens:
+        print("No user tokens available.")
+        return
+
+    notification = messaging.Notification(
+        title=title,
+        body=body,
+        image=image_url
     )
-    response = messaging.send(message)
-    print("Successfully sent message:", response)
+
+    if topic:
+        topic = f"/topics/{topic.replace(' ', '_').lower()}"
+        message = messaging.Message(
+            notification=notification,
+            topic=topic,
+        )
+        try:
+            response = messaging.send(message)
+            print(f"Successfully sent message to topic {topic}: {response}")
+        except Exception as e:
+            logging.error(f"Error sending message to topic {topic}: {e}")
+
+    failed_tokens = []
+    for token in tokens:
+        message = messaging.Message(
+            notification=notification,
+            token=token,
+        )
+        try:
+            response = messaging.send(message)
+            print(f"Successfully sent message to {token}: {response}")
+        except Exception as e:
+            logging.error(f"Error sending message to {token}: {e}")
+            failed_tokens.append(token)
+
+    if failed_tokens:
+        print('List of tokens that caused failures: {0}'.format(failed_tokens))
 
 
 @app.task(name="send_event_notification")
